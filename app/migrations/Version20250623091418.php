a8d42beb4fa5e25f955b3aa684068569de9b2e2d
dev.aleksey.ch <dev.aleksey.ch@alldigitalads.com>
2025-06-23 14:57:31 +0300

    add parent task

---
+++ app/migrations/Version20250623091418.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250623091418 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE tasks DROP FOREIGN KEY FK_50586597727ACA70
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP INDEX IDX_50586597727ACA70 ON tasks
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE tasks DROP parent_id, CHANGE updated_at completed_at DATETIME DEFAULT NULL
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE tasks ADD parent_id INT DEFAULT NULL, CHANGE completed_at updated_at DATETIME DEFAULT NULL
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE tasks ADD CONSTRAINT FK_50586597727ACA70 FOREIGN KEY (parent_id) REFERENCES tasks (id) ON UPDATE NO ACTION ON DELETE SET NULL
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE INDEX IDX_50586597727ACA70 ON tasks (parent_id)
+        SQL);
+    }
+}
